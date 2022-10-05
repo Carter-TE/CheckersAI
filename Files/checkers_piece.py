55e12c577df0aa52a48181a90dc37809480cfe3d
Carter-TE <cte12001@gmail.com>
2022-10-04 23:44:48 -0700

    deleted calc moves in piece class

--- Files/checkers_piece.py
+++ Files/checkers_piece.py
@@ -20,43 +20,7 @@ class Piece:
     def move(self, space):
         self.pos = space
 
-    def calc_moves(self):
-        """
-        Updates move field with all possible moves the piece can make
 
-        :return:
-        """
-
-        self.moves = []
-        pos_index = int(functions.to_index(self.pos))
-        temp_moves = [pos_index + (9 * self.direction), pos_index + (11 * self.direction)]
-        if self.queen:
-            temp_moves.append(pos_index - (11 * self.direction))
-            temp_moves.append(pos_index - (9 * self.direction))
-
-        # Formatting for moves to 0th row (top of the board)
-        for i in range(len(temp_moves)):
-            if len(str(temp_moves[i])) < 2:
-                temp_moves[i] = '0'+str(temp_moves[i])
-
-        # Filters moves not on board
-        moves = []
-        for move in temp_moves:
-            try:
-                m = functions.to_coordinate(move)
-            except IndexError as err:
-                continue
-            moves.append(m)
-            del (m)
-
-        # Filters valid moves
-        for move in moves:
-            if self.board.can_move_to(move):
-                self.moves.append(move)
-                continue
-            jump = self.can_jump(move)
-            if jump is not None:
-                self.moves.append(jump)
 
     def can_jump(self, move):
         """
